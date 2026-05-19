70e1b29ab8d0ee77079c8e1cc12f46d3658a0ecf
Thusira90 <stxaviertimberoffice@gmail.com>
2026-05-20 00:51:22 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const base = 'https://ceypall.com'
+  const base = 'https://www.ceypall.com'
 
   return [
     {
